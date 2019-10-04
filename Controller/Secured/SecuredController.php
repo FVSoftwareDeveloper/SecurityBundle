f35e73c5177cf161b29b3385925ffe42d9387480
Yansell Rivas <yrivas@SynetekSolutions.local>
2019-10-04 15:04:33 -0400

    Unauthorized response with json format

--- Controller/Secured/SecuredController.php
+++ Controller/Secured/SecuredController.php
@@ -27,6 +27,7 @@ class SecuredController extends Controller {
      * @Route("/denied", name="nti_secured_denied")
      */
     public function denied(Request $request) {
+        if (0 === strpos($request->headers->get('Content-Type'), 'application/json'))return new JsonResponse(array("message"=> "access denied"), 403);
         $template = $this->container->hasParameter("nti_security.error_template") ? $this->getParameter("nti_security.error_template") : "NTISecurityBundle:Security:error403.html.twig";
         return $this->render($template);
     }
