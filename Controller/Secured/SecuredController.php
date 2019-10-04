015deb249cb8304e69035358a56931af474ae311
Yansell Rivas <yrivas@SynetekSolutions.local>
2019-10-04 15:33:23 -0400

    use statement fix

--- Controller/Secured/SecuredController.php
+++ Controller/Secured/SecuredController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Class SecuredController
